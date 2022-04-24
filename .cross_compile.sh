381c7d8790dded843985115422638d0778c0905f
Vincent Young <missuo@pm.me>
2022-04-24 11:27:33 +0800

    Update .cross_compile.sh

--- .cross_compile.sh
+++ .cross_compile.sh
@@ -5,7 +5,7 @@ set -e
 DIST_PREFIX="BetterTrace"
 DEBUG_MODE=${2}
 TARGET_DIR="dist"
-PLATFORMS="darwin/amd64 darwin/arm64 linux/386 linux/amd64 linux/arm linux/arm64 windows/386 windows/amd64"
+PLATFORMS="darwin/amd64 darwin/arm64 linux/amd64 linux/arm64"
 
 rm -rf ${TARGET_DIR}
 mkdir ${TARGET_DIR}
