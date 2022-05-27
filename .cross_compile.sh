d690f680f56228762a015da94f29fca9e328a3c7
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2022-05-28 02:29:18 +0800

    增加一些平台支持

--- .cross_compile.sh
+++ .cross_compile.sh
@@ -5,7 +5,7 @@ set -e
 DIST_PREFIX="nexttrace"
 DEBUG_MODE=${2}
 TARGET_DIR="dist"
-PLATFORMS="darwin/amd64 darwin/arm64 linux/amd64 linux/arm64 linux/mips linux/arm/v7 openbsd/amd64 freebsd/amd64"
+PLATFORMS="darwin/amd64 darwin/arm64 linux/386 linux/amd64 linux/arm64 linux/mips openbsd/amd64 openbsd/arm64 freebsd/amd64 freebsd/arm64"
 
 BUILD_VERSION="$(git describe --tags --always)"
 BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
@@ -17,9 +17,7 @@ mkdir ${TARGET_DIR}
 for pl in ${PLATFORMS}; do
     export GOOS=$(echo ${pl} | cut -d'/' -f1)
     export GOARCH=$(echo ${pl} | cut -d'/' -f2)
-    export GOARM=$(echo ${pl} | cut -d'v' -f2)
     export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}
-    [[ `echo ${pl} | cut -d'v' -f2` == '' ]] && export GOARM="" || export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}_v${GOARM}
     if [ "${GOOS}" == "windows" ]; then
         export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}.exe
     fi
@@ -39,4 +37,24 @@ for pl in ${PLATFORMS}; do
                         -w -s"
     fi
 done
+
+    export GOOS='linux'
+    export GOARCH='arm'
+    export GOARM='7'
+    export TARGET=${TARGET_DIR}/${DIST_PREFIX}_${GOOS}_${GOARCH}v7
+    echo "build => ${TARGET}"
+    if [ "${DEBUG_MODE}" == "debug" ]; then
+        go build -trimpath -gcflags "all=-N -l" -o ${TARGET} \
+            -ldflags    "-X 'github.com/xgadget-lab/nexttrace/printer.version=${BUILD_VERSION}' \
+                        -X 'github.com/xgadget-lab/nexttrace/printer.buildDate=${BUILD_DATE}' \
+                        -X 'github.com/xgadget-lab/nexttrace/printer.commitID=${COMMIT_SHA1}'\
+                        -w -s"
+    else
+        go build -trimpath -o ${TARGET} \
+            -ldflags    "-X 'github.com/xgadget-lab/nexttrace/printer.version=${BUILD_VERSION}' \
+                        -X 'github.com/xgadget-lab/nexttrace/printer.buildDate=${BUILD_DATE}' \
+                        -X 'github.com/xgadget-lab/nexttrace/printer.commitID=${COMMIT_SHA1}'\
+                        -w -s"
+    fi
+
 
