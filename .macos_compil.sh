c4ea506c35f4b2c5aaee89797f664ac405871bac
zhshch2002 <zhshch@athorx.com>
2022-06-06 10:32:09 +0800

    update: .macos_compil.sh

--- .macos_compil.sh
+++ .macos_compil.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-BUILD_VERSION="$(git describe --tags --always)"
+BUILD_VERSION=$1
 BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
-COMMIT_SHA1="$(git rev-parse --short HEAD)"
+COMMIT_SHA1=""
 
 go build -trimpath -ldflags "-X 'github.com/xgadget-lab/nexttrace/printer.version=${BUILD_VERSION}' \
                              -X 'github.com/xgadget-lab/nexttrace/printer.buildDate=${BUILD_DATE}' \
