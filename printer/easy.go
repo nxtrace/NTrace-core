b88d5292da1deeab6a56a2ae95908aa9a571a134
Leo <i@leo.moe>
2023-05-23 20:00:42 +0800

    chore: Classic Printer Change

---
+++ printer/easy.go
@@ -0,0 +1,17 @@
+package printer
+
+import (
+	"fmt"
+
+	"github.com/xgadget-lab/nexttrace/trace"
+)
+
+func EasyPrinter(res *trace.Result, ttl int) {
+	for i := range res.Hops[ttl] {
+		if res.Hops[ttl][i].Address == nil {
+			fmt.Printf("%d|*||||||\n", ttl+1)
+			continue
+		}
+		fmt.Printf("%d|%s|%s|%s|%s|%s|%s|%s|%s|%.4f|%.4f\n", ttl+1, res.Hops[ttl][i].Address.String(), res.Hops[ttl][i].Hostname, res.Hops[ttl][i].Geo.Asnumber, res.Hops[ttl][i].Geo.Country, res.Hops[ttl][i].Geo.Prov, res.Hops[ttl][i].Geo.City, res.Hops[ttl][i].Geo.District, res.Hops[ttl][i].Geo.Owner, res.Hops[ttl][i].Geo.Lat, res.Hops[ttl][i].Geo.Lng)
+	}
+}
