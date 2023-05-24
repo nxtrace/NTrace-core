ac1ee5e08c1366d17e183a88c6982426ed830d25
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-05-24 16:08:59 +0800

    增加tracemap的异常处理 防止与tracemap连接出现问题时报错

--- tracemap/tracemap.go
+++ tracemap/tracemap.go
@@ -11,8 +11,17 @@ import (
 
 func GetMapUrl(r string) {
 	url := "https://api.leo.moe/tracemap/api"
-	resp, _ := http.Post(url, "application/json", strings.NewReader(r))
-	body, _ := io.ReadAll(resp.Body)
+	resp, err := http.Post(url, "application/json", strings.NewReader(r))
+	if err != nil {
+		fmt.Println("An issue occurred while connecting to the tracemap API.")
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("An issue occurred while connecting to the tracemap API.")
+		return
+	}
 	fmt.Fprintf(color.Output, "%s %s\n",
 		color.New(color.FgWhite, color.Bold).Sprintf("%s", "MapTrace URL:"),
 		color.New(color.FgBlue, color.Bold).Sprintf("%s", string(body)),
